781cca17d8a556466ed4ff7fafdd131b10124cb8
MrDark <arhammurtaza666@gmail.com>
2024-10-14 17:11:53 +0500

    Add 10oct2024 files

---
+++ 10oct2024/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 20,
+        autoplay: true,
+        center: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        autoHeight: true,
+    });
+});
