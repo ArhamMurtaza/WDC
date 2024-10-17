0e9b8eade83c44ff2f04a9d80ace0ee02982828e
Dark <arhammurtaza@proton.me>
2024-10-17 06:10:57 -0700

    add 17oct2024 files

---
+++ 17oct2024/js/main.js
@@ -0,0 +1,16 @@
+var a = 15;
+var b = 2;
+
+ // arithematic operators
+document.getElementById('plus').textContent = a+b;
+document.getElementById('minus').textContent = a-b;
+document.getElementById('multiply').textContent = a*b;
+document.getElementById('divide').textContent = a/b;
+document.getElementById('power').textContent = a**b;
+document.getElementById('modulus').textContent = a%b;
+
+// assignment operators
+document.getElementById('plus-plus').textContent = ++a;
+document.getElementById('minus-minus').textContent = --a;
+document.getElementById('plus-equal').textContent = a+=5;
+document.getElementById('minus-equal').textContent = a-=10;
